2cc058887c18dd459b165feea22e398f64603a13
jbmensah <jaybmensah@gmail.com>
2024-09-28 05:09:53 +0000

    Token Authentication...you can implement expiration

--- py_client/create.py
+++ py_client/create.py
@@ -1,11 +1,12 @@
 import requests
 
+headers = {'Authorization': 'Bearer 80d48ec3de9d2d9d3d9f8424a9a99178143d7a65'}
 endpoint = "http://localhost:8000/api/products/" #http://127.0.0.1:8000
 
 data = {
-	"title": "Hello, CREATE MODEL MIXIN.",
-	"price": 22.99
+	"title": "Hello, with old token.",
+	"price": 20.00
 }
 
-get_response =  requests.post(endpoint, json=data) 
+get_response =  requests.post(endpoint, json=data, headers=headers) 
 print(get_response.json())
--- py_client/list.py
+++ py_client/list.py
@@ -1,6 +1,19 @@
 import requests
+from getpass import getpass
 
-endpoint = "http://localhost:8000/api/products/" #http://127.0.0.1:8000
+auth_endpoint = "http://localhost:8000/api/auth/" #http://127.0.0.1:8000
+username = input("What is your username?\n")
+password = getpass("Enter your password\n")
 
-get_response =  requests.get(endpoint) 
+auth_response =  requests.post(auth_endpoint, json={"username": username, "password": password}) 
+print(auth_response.json())
+
+if auth_response.status_code == 200:
+	token = auth_response.json()['token']
+	headers = {
+		"Authorization": f"Bearer {token}" # "Token token"
+	}
+	endpoint = "http://localhost:8000/api/products/" #http://127.0.0.1:8000
+
+get_response =  requests.get(endpoint, headers=headers) 
 print(get_response.json())
