1f1dc6045fe2f22c00307dc9e168e329392124e1
jbmensah <jaybmensah@gmail.com>
2024-09-30 00:30:59 +0000

    Related Fields & Foreign Key Serializer

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,23 +1,37 @@
 from rest_framework import serializers
 from rest_framework.reverse import reverse
 
+from api.serializers import UserPublicSerializer
+
 from .models import Product
 from . import validators
 
+
+class ProductInlineSerializer(serializers.Serializer):
+	url = serializers.HyperlinkedIdentityField(
+		view_name='product-detail',
+		lookup_field='pk',
+		read_only=True
+	)
+	title = serializers.CharField(read_only=True)
+
 class ProductSerializer(serializers.ModelSerializer):
+	owner = UserPublicSerializer(source='user', read_only=True)
+	related_products = ProductInlineSerializer(source='user.product_set.all', read_only=True, many=True)
+	my_user_data = serializers.SerializerMethodField(read_only=True)
 	my_discount = serializers.SerializerMethodField(read_only=True)
 	edit_url = serializers.SerializerMethodField(read_only=True)
 	url = serializers.HyperlinkedIdentityField(
 		view_name='product-detail',
 		lookup_field='pk')
-	# email = serializers.EmailField(write_only=True)
+	# email = serializers.EmailField(source='user.email', read_only=True)
 	title = serializers.CharField(validators=[validators.validate_title_no_hello, validators.unique_product_title])
 	# name = serializers.CharField(source='title', read_only=True)
 
 	class Meta:
 		model = Product
 		fields = [
-			# 'user',
+			'owner',
 			'url',
 			'edit_url',
 			# 'email',
@@ -27,9 +41,17 @@ class ProductSerializer(serializers.ModelSerializer):
 			'content',
 			'price',
 			'sale_price',
-			'my_discount'
+			'my_discount',
+			'my_user_data',
+			'related_products'
 		]
 
+	def get_my_user_data(self, obj):
+		return {
+			"username":obj.user.username
+		}
+
+	# def validate(self, data):
 	def validate_title(self, value):
 		# if len(value) < 10:
 		# 	raise serializers.ValidationError("Title is too short!")
